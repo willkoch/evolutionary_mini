25b97285249d4f1d7361c4940b2d74ea9836d9e9
kochwill <wkoch530@gmail.com>
2020-02-11 14:55:14 -1000

    progress

--- mini.py
+++ mini.py
@@ -3,3 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#Define a search space — something simple like integers, binary, reals, vectors, etc. is fine — or your choice!
+
+#finding factors of a number? 
+
+
+
+
+#Define an objective function on the search space — something easily implemented in an algorithm.
+
+
+
+
+
+#Define variation operators on elements of the search space — mutation, recombination, etc., something easily implemented in an algorithm.
+
+
+
+
+
+#Decide on a selection operator — fitness proportional to the objective function, tournament, rank order, truncation, or your choice.
+
+
+
+
